4016939b92fb52bcc3fb06e41c9fb3c2c1344665
littleguga <fed777os@gmail.com>
2016-07-02 02:54:39 +0300

    update link to docs

--- extension/popup/js.js
+++ extension/popup/js.js
@@ -1,6 +1,6 @@
 var links = {
     github: 'https://github.com/littleguga/steam-cards-getter',
-    docs: 'https://github.com/littleguga/steam-cards-getter/',
+    docs: 'https://github.com/littleguga/steam-cards-getter/blob/master/README.md',
     start: 'http://store.steampowered.com/explore/'
 };
 
